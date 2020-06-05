2906dc27a795c02172084cbf5ded1dc53a2c715f
Prudvidhar Reddy Ravi <pravi@isimtl.local>
2020-06-05 18:10:42 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
+import Todos from './components/Todos';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+ state= {
+   todos : [
+     {
+       id:1,
+       title:'take out the trash',
+       completed : false
+     },
+     {
+      id:2,
+      title:'Dinner with gf',
+      completed : true
+    },
+    {
+      id:3,
+      title:'Meeting with boss',
+      completed : false
+    },
+   ]
+ } 
+ // Completed todo
+ markComplete=(id)=>{
+   this.setState({todos:this.state.todos.map(todo=>{
+   if(todo.id===id){
+     todo.completed=!todo.completed
+   }
+   return todo;
+   })});
+ }
+
+ //Delete Todo
+deleteTodo=(id)=>{
+  console.log(id)
 }
+
+
+  render(){
+    console.log(this.state.todos)
+    return (
+      <div className="App">
+        <Todos  todos={this.state.todos} markComplete={this.markComplete} deleteTodo={this.deleteTodo}/>
+      </div>
+    );
+  }
+  }
+
 
 export default App;
